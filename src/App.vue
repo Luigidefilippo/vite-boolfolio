687b3d09804c2e3f27e7a1d5d7e0c9ec78fb76ec
Luigidefilippo <defilippoluigi65@gmail.com>
2023-06-13 17:24:01 +0200

    vue project done

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
     getProjects(){
       console.log('get Project');
       axios.get('http://127.0.0.1:8000/api/projects').then(resp =>{
-        console.log(resp)
+        this.projects = resp.data.results;
       })
     }
   }
